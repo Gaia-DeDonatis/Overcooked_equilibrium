b1ece94dcd13545c99c051b677287540b939f04d
Gaia De Donatis <gaia.dedonatis.mail@gmail.com>
2026-02-09 11:21:15 +0200

    files

--- Equilibrium_project/gym_macro_overcooked/__init__.py
+++ Equilibrium_project/gym_macro_overcooked/__init__.py
@@ -1,42 +1,12 @@
 from gym.envs.registration import register
 
+
 register(
-    id='Overcooked-v0',
-    entry_point='gym_macro_overcooked.overcooked:Overcooked',
+    id='Overcooked-equilibrium-v0',
+    entry_point='gym_macro_overcooked.overcooked_equilibrium:Overcooked_equilibrium',
 )
 
-# mapA
 register(
-    id='Overcooked-MA-v0',
-    entry_point='gym_macro_overcooked.overcooked_MA:Overcooked_MA',
-)
-
-# mapA_ban
-register(
-    id='Overcooked-MA-v3',
-    entry_point='gym_macro_overcooked.overcooked_MA_ban_get_plate:Overcooked_MA_ban_get_plate',
-)
-
-# mapB, mapB_lowuncertainty
-register(
-    id='Overcooked-MA-v1',
-    entry_point='gym_macro_overcooked.overcooked_MA_mapB:Overcooked_MA_mapB',
-)
-
-# mapB_ban, mapB_lowuncertainty_ban
-register(
-    id='Overcooked-MA-v4',
-    entry_point='gym_macro_overcooked.overcooked_MA_mapB_ban_get_plate:Overcooked_MA_mapB_ban_get_plate',
-)
-
-# mapA_lowuncertainty
-register(
-    id='Overcooked-MA-v2',
-    entry_point='gym_macro_overcooked.overcooked_MA_lowuncertainty:Overcooked_MA_lowuncertainty',
-)
-
-# mapA_lowuncertainty_ban
-register(
-    id='Overcooked-MA-v5',
-    entry_point='gym_macro_overcooked.overcooked_MA_lowuncertainty_ban_get_plate:Overcooked_MA_lowuncertainty_ban_get_plate',
+    id='Overcooked-MA-equilibrium-v0',
+    entry_point='gym_macro_overcooked.overcooked_MA_equilibrium:Overcooked_MA_equilibrium',
 )
