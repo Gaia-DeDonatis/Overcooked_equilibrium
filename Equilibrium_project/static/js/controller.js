84c313ae0c3e3ca4ef13a0e36457177a2922aaba
Gaia De Donatis <gaia.dedonatis.mail@gmail.com>
2026-02-27 09:50:18 +0200

    controller //STATE.configId

--- Equilibrium_project/static/js/controller.js
+++ Equilibrium_project/static/js/controller.js
@@ -171,7 +171,7 @@ async function startRound({ newEpisode = false } = {}) {
 
     if (data.map_type) {
       STATE.assignment.layout = data.map_type;
-      STATE.configId = data.map_type + '_experiment';
+      //STATE.configId = data.map_type + '_experiment';
     }
 
     if (DataManager.LOGS.meta.tick_ms == null) {
